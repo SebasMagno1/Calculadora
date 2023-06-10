036652863e1673612c4f0035cd734a2cef8e0040
SebasMagno1 <114171307+SebasMagno1@users.noreply.github.com>
2023-06-09 23:28:25 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sebastian Escobar',
+    author_email='',
+    description=''
+)
